c701c066122e97aae0fb588cfbb63a14a143ee8d
Farnerud <hperea.alfa@gmail.com>
2018-05-24 11:23:14 -0500

    adding the responsive classes, files and organizing the html elements.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,11 +3,12 @@ postcss = require("gulp-postcss"),
 autoprefixer = require("autoprefixer"),
 cssvars = require("postcss-simple-vars"),
 cssnested = require("postcss-nested"),
-cssimport = require("postcss-import");
+cssimport = require("postcss-import"),
+mixins = require("postcss-mixins");
 
 gulp.task("styles", function() {
     return gulp.src("./app/assets/styles/styles.css")
-        .pipe(postcss([cssimport, cssvars, cssnested, autoprefixer]))
+        .pipe(postcss([cssimport, mixins, cssvars, cssnested, autoprefixer]))
         .on("error", function(errorMsg){
         	console.log(errorMsg.toString());
         	this.emit("end");
